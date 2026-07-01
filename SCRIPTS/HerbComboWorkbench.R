f51fc57c15f7c20ca7111be6866aeec020420f7e
22lholl <22lholl@gmail.com>
2026-07-01 00:07:41 -0500

    working on getting comparison table for checking variable datatypes

--- SCRIPTS/HerbComboWorkbench.R
+++ SCRIPTS/HerbComboWorkbench.R
@@ -5,115 +5,101 @@
 
 #what checks did I do for SUWS to be cleaned?
 
-#check if all NAs in a column
-#checking if there is the same entry in every column
-#removing columns of little value to analysis
-#looking for duplicate catalog numbers
-#look for misspellings of collector names
-#make decision about records with incomplete geographic or temporal information
-#separating out and attaching collectors in individual columns
+  #check if all NAs in a column
+  #checking if there is the same entry in every column
+  #removing columns of little value to analysis (trying to reduce file size as much as possible)
+  #look for misspellings of collector names
+  #separating out and attaching collectors in individual columns
+  
+  #looking for duplicate catalog numbers
+  #make decision about records with incomplete geographic or temporal information
 
 #What are additional checks I should do when dealing with multiple herbaria and multiple download sources for herbaria?
 
-#compare datasets between different sources and determine which records to use
-#write code so it is easy to add chopped up large herbaria datasets to be assembled in R for analysis
-#look for any dropped years in the larger datasets that are chopped up by year
+  #compare datasets between different sources and determine which records to use
+  #write code so it is easy to add chopped up large herbaria datasets to be assembled in R for analysis
+  #look for any dropped years in the larger datasets that are chopped up by year
 
 #what constitutes a duplicate?
 
-#same day, same plant, same people
+  #same day, same plant, same people
 
 ####Task: Merge raw csv files into one big dataframe####
+
+#finding which variables are tripping up HerbCombo and giving warning message
+
+# 1. Define the path to your folder
+folder_path <- "DATA/RAW/"
+
+# 2. Get a list of all CSV files in that folder
+file_list <- list.files(path = folder_path, pattern = "\\.csv$", full.names = TRUE)
+
+# 3. Loop through files and assign each to its own data frame
+for (file in file_list) {
+  # Extract the clean file name without the path and extension to use as the variable name
+  obj_name <- tools::file_path_sans_ext(basename(file))
+  
+  # Read the CSV and assign it to that name
+  assign(obj_name, read.csv(file,
+                            fileEncoding = "latin1"))
+}
+
+#delete objects form environment that aren't dataframes
+rm(file)
+rm(file_list)
+rm(folder_path)
+rm(obj_name)
+
+library(janitor)
+
+# 2. Put your data frames into a named list
+df_list <- list(mget(ls()))
+
+# 3. Compare the columns
+comparison_table <- 
+  compare_df_cols(df_list[[1]]) %>%
+  pivot_longer(
+    cols = !c(column_name),      # Excludes these columns from pivoting
+    names_to = "csv_name",            # Name of the new category column
+    values_to = "datatype"            # Name of the new data values column
+  ) %>%
+  group_by(column_name,
+           datatype) %>%
+  count() %>%
+  filter(n < 23)
+
+#process
+  #run comparison table to get list of what variables to check
+  #run comparison table only through pivot longer to include dataset names
+  #compare datasets and HerbCombo entries to see if data dropped
+
+write.csv(comparison_table,
+          file = "DATA/CLEAN/comparison_table.csv",
+          row.names = FALSE)
+
+HerbCombo_datatype_comparison <-
+  HerbCombo %>%
+  select(Source_File,
+         accessRights) %>%
+  group_by(Source_File,
+           accessRights) %>%
+  count()
+
+
+
 
 HerbCombo <- list.files(path = "DATA/RAW",
                         pattern = "\\.csv$",
                         full.names = TRUE) %>%
   setNames(basename(.)) %>%
-  lapply(read_csv, 
-         col_types = cols(.default = col_character()),
+  lapply(read_csv,
+         col_types = cols(.default = "c"),
          locale = locale(encoding = "latin1")) %>%
   bind_rows(.id = "Source_File")
 
 ####Task: Get one record for every record####
 
 #duplicates could happen for two reasons - problem with database having two records or problem that the same record is coming from different databases
-
-#make list of duplicate instances - n more than one indicates it is replicated somewhere, and with source file included this is indicating problems within specific datasets.
-
-lkg_fr_dups <- 
-  HerbCombo %>% 
-  group_by(Source_File,
-           institutionCode,
-           catalogNumber) %>% 
-  count() %>% 
-  filter(n > 1)
-
-#sorting by the largest number reveals datasets where the catalogNumber is NA. These can be checked for unique instances by grouping by date, taxon, and people collecting - chances are, somebody is not going to collect the same plant on the same day, though if this happens geography could be an additional check?
-
-lkg_fr_cN_NA <-
-  HerbCombo %>%
-  filter(is.na(catalogNumber) == TRUE) %>%
-  group_by(Source_File,
-           institutionCode,
-           year,
-           month,
-           day,
-           recordedBy,
-           taxonID) %>%
-  count() %>%
-  filter(n > 1)
-
-#verify that records ok
-
-#maybe cross ref with occurenceID? if occurenceID unique, will be unique record?
-
-#pull out unique instances so that can add independently to dataset later
-  
-
-
-
-
-#Thought process
-
-#sort by collection ID - look at console output to see if more than one listed
-#If only one listed, put into new mega dataset and remove from old
-#If more than one, look at records - try to keep all of one dataset together, but also go for the most complete data
-
-lkg_fr_dups <- 
-  HerbCombo %>% 
-  group_by(Source_File,
-           institutionCode, 
-           catalogNumber) %>% 
-  count() %>% 
-  filter(n > 1)
-
-#seems like if 2 with same catalogNumber from dataset and same institution, might be determination history error? those would require more investigation...
-
-#in any case, can deal with records that have no catalog number first. 
-
-issues <-
-  HerbCombo %>%
-  filter(institutionCode == "CONCOL",
-         catalogNumber == "CONC004877" | catalogNumber == "CONC004879" | catalogNumber == "CONC004885")
-
-issues <-
-  HerbCombo %>%
-  filter(institutionCode == "CSBSJU",
-         is.na(catalogNumber) == TRUE) %>%
-  group_by(year,
-           month,
-           day,
-           recordedBy,
-           taxonID) %>%
-  count() %>%
-  filter(n > 1)
-
-issues <-
-  HerbCombo %>%
-  group_by(institutionCode,
-           occurrenceID) %>%
-  count() %>%
-  filter(n > 1)
 
 #three ways to look for duplicates
 
@@ -530,8 +516,10 @@ issues_NA_NA_NA <-
   count(-n) 
 
 #tasks for tomorrow
+  #figure out issue with UWL not loading correctly  
   #run code line by line, do math to make sure not dropping records
   #make decisions on what can go into dataset for analysis
+  #recycle code to filter out columns for file size
   #finish annotating flowchart
 
 
